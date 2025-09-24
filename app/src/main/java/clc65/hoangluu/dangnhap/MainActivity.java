6de33eea43f5c714a0a6904e1e363e8f55188437
LuuHocGioi <nguyenluwu@gmail.com>
2025-09-24 11:21:00 +0700

    Đăng nhập

--- app/src/main/java/clc65/hoangluu/dangnhap/MainActivity.java
+++ app/src/main/java/clc65/hoangluu/dangnhap/MainActivity.java
@@ -1,6 +1,11 @@
 package clc65.hoangluu.dangnhap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtUsername, edtPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,27 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        edtUsername  = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = edtUsername.getText().toString().trim();
+                String pass = edtPassword.getText().toString().trim();
+
+
+                if (user.equals("admin") && pass.equals("123")) {
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    intent.putExtra("USERNAME", user);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Sai tài khoản hoặc mật khẩu", Toast.LENGTH_SHORT).show();
+                }
+
+            };
         });
     }
 }
