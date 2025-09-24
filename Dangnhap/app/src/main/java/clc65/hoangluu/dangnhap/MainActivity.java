9f28a1ed921973d0d0720b159fe9eda518cbfa0f
LuuHocGioi <nguyenluwu@gmail.com>
2025-09-24 11:38:10 +0700

    Dangnhap

--- Dangnhap/app/src/main/java/clc65/hoangluu/dangnhap/MainActivity.java
+++ Dangnhap/app/src/main/java/clc65/hoangluu/dangnhap/MainActivity.java
@@ -1,8 +1,11 @@
 package clc65.hoangluu.dangnhap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,5 +31,20 @@ public class MainActivity extends AppCompatActivity {
         edtPassword = findViewById(R.id.edtPassword);
         btnLogin = findViewById(R.id.btnLogin);
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edtUsername.getText().toString();
+                String password = edtPassword.getText().toString();
+                if (username.equals("admin") && password.equals("123")) {
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                            intent.putExtra("Username",username);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"sai tk hoac mat khau", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
