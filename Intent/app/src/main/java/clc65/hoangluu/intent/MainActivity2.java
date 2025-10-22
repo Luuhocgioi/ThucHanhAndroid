e66654214b6d57f6fd828e857c8bbb2f8261356e
LuuHocGioi <nguyenluwu@gmail.com>
2025-10-22 09:47:39 +0700

    Update

--- Intent/app/src/main/java/clc65/hoangluu/intent/MainActivity2.java
+++ Intent/app/src/main/java/clc65/hoangluu/intent/MainActivity2.java
@@ -1,6 +1,9 @@
 package clc65.hoangluu.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    Button btnBack;
+    TextView tvName;
+    void Tim() {
+        btnBack = findViewById(R.id.btnBack);
+        tvName = findViewById(R.id.tvName);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Tim();
+        //Nhận về Intent
+        Intent intentback = getIntent();
+        //Bóc ra
+        String nameNhan = intentback.getStringExtra("name");
+        //Xử lý
+        tvName.setText(nameNhan);
+
+        btnBack.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+            startActivity(intent);
         });
     }
-}
+
+
+    }
