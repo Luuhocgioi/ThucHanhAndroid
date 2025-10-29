96899f5a72560bc27872f9d028d16a724069ad8d
LuuHocGioi <nguyenluwu@gmail.com>
2025-10-29 07:58:12 +0700

    update

--- ThiGK/app/src/main/java/thigk1/nguyenhoangluu/thigk/CongTrinh.java
+++ ThiGK/app/src/main/java/thigk1/nguyenhoangluu/thigk/CongTrinh.java
@@ -1,24 +1,61 @@
 package thigk1.nguyenhoangluu.thigk;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class CongTrinh extends AppCompatActivity {
+
+    LandScapeAdapter landscapeAdapter; // Sửa tên biến
+    ArrayList<Landscape> dsList;
+    RecyclerView rvLandscape;
+    ImageButton btnBackToHome;
+
+
+    void Tim() {
+        rvLandscape = findViewById(R.id.rvLandscape);
+        btnBackToHome = findViewById(R.id.btnBackToHome);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        // Sử dụng layout có chứa RecyclerView của bạn (activity_main5.xml)
         setContentView(R.layout.activity_cong_trinh);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Tim();
+        dsList = getData();
+
+        // 1. Thay đổi LayoutManager thành DỌC (VERTICAL)
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rvLandscape.setLayoutManager(layoutManager);
+
+        landscapeAdapter = new LandScapeAdapter(this, dsList); // Sửa tên biến
+        rvLandscape.setAdapter(landscapeAdapter);
+        btnBackToHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Đóng màn hình hiện tại và quay về
+            }
         });
+    }
+
+    ArrayList<Landscape> getData() {
+        ArrayList<Landscape> dsList = new ArrayList<>();
+        // Cung cấp dữ liệu mới (với 3 tham số)
+        dsList.add(new Landscape("Tiêu đề hoạt động 1", "Thời gian 1", "anhnhatrang1"));
+        dsList.add(new Landscape("Tiêu đề hoạt động 2", "Thời gian 2", "anhnhatrang2"));
+        dsList.add(new Landscape("Tiêu đề hoạt động 3", "Thời gian 3", "anhnhatrang3"));
+        dsList.add(new Landscape("Tiêu đề hoạt động 4", "Thời gian 4", "anhnhatrang4"));
+        return dsList;
     }
 }
--- ThiGK/app/src/main/java/thigk1/nguyenhoangluu/thigk/LandScapeAdapter.java
+++ ThiGK/app/src/main/java/thigk1/nguyenhoangluu/thigk/LandScapeAdapter.java
@@ -1,4 +1,69 @@
 package thigk1.nguyenhoangluu.thigk;
 
-public class LandScapeAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemViewHolder> {
+    Context context;
+    ArrayList<Landscape> dsList;
+
+    public LandScapeAdapter(Context context, ArrayList<Landscape> dsList) {
+        this.context = context;
+        this.dsList = dsList;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        // Sử dụng layout landscape_item.xml
+        View view = inflater.inflate(R.layout.landscape_item, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        Landscape landScape = dsList.get(position);
+
+        // Lấy dữ liệu từ model
+        String tvCaption = landScape.getLandScapeName();
+        String tvTime = landScape.getLandScapeTime(); // Lấy dữ liệu thời gian
+        String image = landScape.getLandScapeImage();
+
+        // Gán dữ liệu vào View
+        holder.textViewTitle.setText(tvCaption);
+        holder.textViewTime.setText(tvTime); // Gán thời gian
+
+        // Lấy và gán ảnh
+        String packageName = holder.itemView.getContext().getPackageName();
+        int resId = holder.itemView.getContext().getResources().getIdentifier(image, "mipmap", packageName);
+        holder.imgView.setImageResource(resId);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dsList.size();
+    }
+
+    class ItemViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgView;
+        TextView textViewTitle, textViewTime; // Đổi tên biến cho khớp với ID mới
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // Ánh xạ View theo ID mới
+            imgView = itemView.findViewById(R.id.imgView);
+            textViewTitle = itemView.findViewById(R.id.tvTitle);
+            textViewTime = itemView.findViewById(R.id.tvTime);
+        }
+    }
 }
